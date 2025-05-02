eaf5f5beeb6153331b3a252b940e751dcad31a1b
Ben Dooks <ben.dooks@codethink.co.uk>
2025-05-02 14:20:35 +0100

    riscv: add cva6/genesysII board
    Add the CVA6 FPGA board support, based on the CVA6-SDK
    
    Co-authored-by: Kevin Eyssartier <kevin.eyssartier@thalesgroup.com>
    Co-authored-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    
    initial kconfig from original uboot

---
+++ include/configs/openhwgroup_cva6_genesysII.h
@@ -0,0 +1,12 @@
+// todo - add license and shit
+
+#ifndef __OPENHWGROUP_GENESYSII_H
+#define __OPENHWGROUP_GENESYSII_H
+
+#include <linux/sizes.h>
+
+#define CONFIG_SYS_SDRAM_BASE	0x80000000
+
+// todo - set UART_BASE and UART_REG_WIDTH
+
+#endif /* __OPENHWGROUP_GENESYSII_H */
